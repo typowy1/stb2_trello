6af0b6a25a1cd00f7678e6fc383560762ce98655
Rafal Lesnicki <lesnickirafal@gmail.com>
2023-10-04 22:44:24 +0200

    add lekcja 13

--- src/test/java/pl/stb2trello/requasts/BaseRequest.java
+++ src/test/java/pl/stb2trello/requasts/BaseRequest.java
@@ -5,7 +5,6 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import pl.stb2trello.secrets.TrelloSecrets;
 
 public class BaseRequest {
 
